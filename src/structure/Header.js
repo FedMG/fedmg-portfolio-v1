67bd68cfb26e70307996f4e8a9d51155c68c9c0c
FedMG <75050231+FedMG@users.noreply.github.com>
2022-09-04 20:57:53 -0300

    Modify: style of the navbar

--- src/structure/Header.js
+++ src/structure/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './items/Nav.js';
 import Logo from './items/Logo.js';
 import CreateList from '../utils/CreateList.js';
+import Dropdown from './items/Dropdown.js';
 import './styles/header.css';
 import './styles/createlist.css';
 
@@ -10,18 +11,18 @@ const Header = () => {
     <header className="Header">
       <Nav classes={['Nav', 'Nav__unordered-list']}>
         <li className="Logo-container">
-            <Logo
-              classes={['Logo-anchor', 'Logo-anchor__image']}
-              href="#home"
-              alt="portfolio logo"
-              source="https://img.icons8.com/nolan/64/folder-invoices.png"
-            />
+          <Logo
+            classes={['Logo-anchor', 'Logo-anchor__image']}
+            href="#Home"
+            alt="portfolio logo"
+            source="https://img.icons8.com/nolan/64/folder-invoices.png"
+          />
         </li>
         <div className="lists-wrapper">
-        <CreateList
-          listNames={['Home', 'About me', 'Projects', 'Skills', 'Contact']}
-          classes={['Lists', 'Lists__anchor']}
-        />
+          <CreateList
+            listNames={['Home', 'About', 'Projects', 'Skills', 'Contact']}
+            classes={['Lists', 'Lists__anchor']}
+          />
         </div>
       </Nav>
     </header>
@@ -29,3 +30,21 @@ const Header = () => {
 };
 
 export default Header;
+
+/* 
+<Dropdown
+href="javascript:void(0)"
+classes={[
+  'Dropdown',
+  'dropdown__anchor',
+  'dropdown__icon',
+  'dropdown__items',
+]}
+source="https://img.icons8.com/ios-filled/50/000000/menu--v1.png"
+>
+<CreateList
+  listNames={['Home', 'About', 'Projects', 'Skills', 'Contact']}
+  classes={['Dropdown__Lists', 'Dropdown-Lists__anchor']}
+/>
+</Dropdown>
+*/
