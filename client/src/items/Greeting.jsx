5c2a09338e61e45091332cc253eced34ee8199df
FedMG <federico.gonzalia@gmail.com>
2022-10-15 14:04:58 +0000

    Refactor: /Home.jsx, and Create: Greeting, Introduction & Typography components

---
+++ client/src/items/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "./Typography.jsx";
+
+export const Greeting = () => {
+  return (
+    <div className="greeting-container">
+      <Typography style="greeting-container__span" as="span">
+        Hello world!
+      </Typography>
+    </div>
+  );
+};
---
+++ client/src/items/Introduction.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Typography } from "./Typography.jsx";
+
+export const Introduction = () => {
+  return (
+    <div className="span-container">
+      <div className="span-container__subcontainer">
+        <Typography style="first-phrase" as="span">
+          My name is{" "}
+          <Typography style="first-phrase__span" as="span">
+            Federico
+          </Typography>
+        </Typography>
+        <br />
+        <Typography style="second-phrase" as="span">
+          I'm a{" "}
+          <Typography style="second-phrase__span" as="span">
+            Fullstack Developer
+          </Typography>
+        </Typography>
+        <br />
+        <Typography style="third-phrase" as="span">
+          A passionate of the{" "}
+          <Typography style="third-phrase__span" as="span">
+            IT World
+          </Typography>
+          .
+        </Typography>
+        <br />
+      </div>
+    </div>
+  );
+};
