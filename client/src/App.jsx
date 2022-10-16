5f84edfaf8c72a7cf4f1a22695f8378f2f23cb86
FedMG <federico.gonzalia@gmail.com>
2022-10-16 02:59:52 +0000

    Remove: index.css and move to global.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
-// import * as React from 'react';
-import { useEffect } from "react";
 import Header from "./structure/Header.jsx";
 import Main from "./structure/Main.jsx";
 import "./styles/globals.css";
 
 function App() {
-  // useEffect();
-
   return (
     <div className="App">
       <Header />
--- client/src/routes/Contact.jsx
+++ client/src/routes/Contact.jsx
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
-
-// const Mailto = ({ email, subject = "", body = "", children }) => {
-//   let params = subject || body ? "?" : "";
-//   if (subject) params += `subject=${encodeURIComponent(subject)}`;
-//   if (body) params += `${subject ? "&" : ""}body=${encodeURIComponent(body)}`;
-
-//   return <a href={`mailto:${email}${params}`}>{children}</a>;
-// };
+import React from 'react';
 
 const Contact = () => {
-  // const [message, setMessage] = useState();
-
   return (
     <div id="Contact" className="section">
       <h2 className="subtitle-section">
         <span className="span-subtitle">Contact</span> me
       </h2>
-      {/* <input />
-      <input />
-      <textarea /> */}
-      <div>
-        {/* <Mailto email="" subject="" body="">
-          email me
-        </Mailto> */}
-      </div>
     </div>
   );
 };
