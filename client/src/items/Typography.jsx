67420aaf71e25fcc7f24b084235441a428a24431
FedMG <federico.gonzalia@gmail.com>
2022-10-17 23:57:48 +0000

    Add: dynamic functionallity in Typography to choose tags

--- client/src/items/Typography.jsx
+++ client/src/items/Typography.jsx
@@ -1,5 +1,20 @@
-export const Typography = ({ style, as, children }) => {
-  if (as === "span") {
-    return <span className={style}>{children}</span>;
-  }
+import { createElement } from "react";
+
+const elements = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  h5: "h5",
+  h6: "h6",
+  span: "span",
+  p: "p",
+};
+
+export const Typography = ({ as = "span", style, children }) => {
+  return createElement(
+    elements[as] || elements.span,
+    { className: style },
+    children
+  );
 };
