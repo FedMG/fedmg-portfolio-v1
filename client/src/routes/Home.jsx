4f22df542ddc134d1329e68ffcee2fb074869dd9
FedMG <federico.gonzalia@gmail.com>
2022-11-27 10:32:31 +0000

    Apply: new home layout

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,18 +1,23 @@
-import { Introduction } from '../items/home/Introduction.jsx'
-import { Greeting } from '../items/home/Greeting.jsx'
-import { LinkSection } from '../items/home/LinkSection.jsx'
-import { Shape } from '../iterators/Shape.jsx'
-import { waves } from '../refs/shapes.js'
+import { Introduction } from "../items/home/Introduction.jsx";
+import { LinkSection } from "../items/home/LinkSection.jsx";
+import { Shape } from "../iterators/Shape.jsx";
+import { waves } from "../refs/shapes.js";
+import intro from "../assets/intro.gif";
 
 export const Home = () => {
   return (
-    <div id='Home' className='section'>
-      <div className='home-section-content'>
-        <Greeting />
-        <Introduction />
-        <LinkSection />
+    <div id="Home" className="section">
+      <div className="home__sticky-header" />
+      <div className="home__elements-wrapper">
+        <div className="intro-btn-elements">
+          <Introduction />
+          <LinkSection />
+        </div>
+        <div className="intro-gif-container">
+          <img src={intro} className="intro-gif" />
+        </div>
       </div>
       <Shape data={waves.upside} />
     </div>
-  )
-}
+  );
+};
---
+++ client/src/structure/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div>
+      <a href='https://storyset.com/online'>Online illustrations by Storyset</a>
+    </div>
+  )
+}
