d4c47ffabfdd693ba3be703595298763c805c78a
FedMG <federico.gonzalia@gmail.com>
2022-11-28 16:54:42 +0000

    Update: Header and separate Nav to be sticky

--- client/src/structure/Header.jsx
+++ client/src/structure/Header.jsx
@@ -4,14 +4,16 @@ import { headerData } from '../refs/headerData.js'
 
 export const Header = () => {
   return (
-    <header className='Header'>
-      <Top title='Fed' span='MG' />
+    <>
+      <header className='Header'>
+        <Top title='Fed' span='MG' />
+      </header>
       <nav className='navbar'>
         <CreateList
           listNames={headerData.routeNames}
           classes={headerData.styles}
         />
       </nav>
-    </header>
+    </>
   )
 }
