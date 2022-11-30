d0f4136074f6b4e9e40f6495cf9847c93583a367
FedMG <federico.gonzalia@gmail.com>
2022-11-30 14:19:29 +0000

    Create: addId function in /tools.js, remove useless comments

--- client/src/functions/tools.js
+++ client/src/functions/tools.js
@@ -6,3 +6,16 @@ export const setAnimation = (containerStyle, transition, transform) => {
   containerStyle.transition = transition
   containerStyle.transform = transform
 }
+
+export const addId = (array, id = 0, len = array.length) =>  {
+  if (!Array.isArray(array)){
+    return 'Is not an Array'
+  } 
+  
+  if (id >= len) {
+    return array
+  } else {
+    array[id]['id'] = id;
+    return run(array, id + 1)
+  }
+}
