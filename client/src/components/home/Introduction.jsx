c26ce42900e60b6681697d6e8840e70509570959
FedMG <federico.gonzalia@gmail.com>
2022-12-08 22:21:27 +0000

    Create:  Typewriters and custom hooks to use in Introduction

--- client/src/components/home/Introduction.jsx
+++ client/src/components/home/Introduction.jsx
@@ -1,32 +1,29 @@
-import { Typography } from '@/components/Typography'
+import { Typography } from "@/components/Typography";
+import { Typewriter, TypewriterSplit } from "./Typewriter";
 
 export const Introduction = () => {
   return (
-    <div className='span-container'>
-      <div className='span-container__subcontainer'>
+    <div className="span-container">
+      <div className="span-container__subcontainer">
         <Typography style='first-phrase' as='span'>
           Hi, I am{' '}
           <Typography style='first-phrase__span' as='span'>
             Federico
           </Typography>
         </Typography>
-        <br />
         <Typography style='second-phrase' as='span'>
           A{' '}
           <Typography style='second-phrase__span' as='span'>
             Fullstack Developer
           </Typography>
         </Typography>
-        <br />
-        <Typography style='third-phrase' as='span'>
-          And a passionate of the{' '}
-          <Typography style='third-phrase__span' as='span'>
+        <Typography style="third-phrase" as="span">
+          And a passionate of the{" "}
+          <Typography style="third-phrase__span" as="span">
             Information Technology World
           </Typography>
-
         </Typography>
-        <br />
       </div>
     </div>
-  )
-}
+  );
+};
---
+++ client/src/hooks/useTypewriterEffect.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+export const useTypewriterEffect = (msg, delay = 120, timeout = 900) => {
+  const [text, setText] = useState("");
+  const [next, setNext] = useState(0);
+  
+  useEffect(() => {
+    let index = 0;
+
+    if (index <= text.length) {
+      const interval = setInterval(() => {
+        setText(msg[next].substring(0, index));
+        index++;
+
+        if (index > msg[next].length) {
+          clearInterval(interval);
+        }
+      }, delay);
+    }
+  }, [next]);
+
+  useEffect(() => {
+    if (text.length === msg[next].length) {
+
+      const prevRemove = setTimeout(() => {
+
+        let index = text.length;
+
+        const interval = setInterval(() => {
+          index--;
+          setText(msg[next].substring(0, index));
+          
+          if (index <= 0) {
+            setNext(next => next + 1)
+            
+            if (next >= msg.length -1) {
+              setNext(0)
+            }
+            clearInterval(interval);
+          }
+        }, delay);
+      }, timeout);
+      return () => clearTimeout(prevRemove);
+    }
+  }, [text]);
+  
+  return [text]
+}
