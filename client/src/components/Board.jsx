eee55b38e30aff1cea90770a8f01f5b62b908b12
FedMG <federico.gonzalia@gmail.com>
2023-01-17 00:37:38 +0000

    update: board text

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,20 +1,19 @@
-import { removeEffect } from "@/functions/board-effect";
+import { removeEffect } from '@/functions/board-effect'
 import { useBoardEffect } from '@/hooks/useBoardEffect'
 
-import { Typography } from "./utils/Typography";
+import { Typography } from './utils/Typography'
 
 export const Board = () => {
   const {
     methods: { getEvent, getTouchEvent },
     object: references,
-    ref: board,
-  } = useBoardEffect();
-  
-  
+    ref: board
+  } = useBoardEffect()
+
   return (
-    <div className="about-board-container grid-center">
+    <div className='about-board-container grid-center'>
       <div
-        className="about-board"
+        className='about-board'
         onMouseMove={getEvent}
         onMouseLeave={() => removeEffect(references)}
         onTouchStart={getTouchEvent}
@@ -22,8 +21,8 @@ export const Board = () => {
         onTouchEnd={() => removeEffect(references)}
         ref={board}
       >
-        <div className="about-card bk-drop">
-          <Typography as="p" style="about-text">
+        <div className='about-card bk-drop'>
+          <Typography as='p' style='about-text'>
             I am a self-taught student of Software Engineering &#128187;, and a
             student at UBA of Data science degree &#128202;
             <br />
@@ -34,27 +33,24 @@ export const Board = () => {
             <br />
             &#127793; Some of my hobbies are studying a new language like
             Japanese, and spending time with my family.
-            <span className="hr"/>
+            <span className='hr' />
             <strong>Grow mindset</strong>
             <br />
-            Me gusta pensar a lo grande, en como hacer algo mejor, como ir más
-            allá.
+            I like to think big, how to make something better, how to go further.
             <br />
             <br />
             <strong>Work ethic</strong>
             <br />
-            Adoro continuar aprendiendo cosas nuevas y seguir mejorando para dar
-            el mejor resultado.
+            I love to keep learning new things and keep improving to give the best result.
             <br />
             <br />
             <strong>Team player attitude</strong>
             <br />
-            Me encanta tarbajar codo a codo y ayudar a quién lo necesite para
-            lograr un objetivo en común.
+            I love to work side by side and help whoever needs it to achieve a common goal.
           </Typography>
-          <div className="background-effect container" />
+          <div className='background-effect container' />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
