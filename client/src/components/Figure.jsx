37099b92ec559ffcccb7d8855d7bf00bc31ced9e
FedMG <federico.gonzalia@gmail.com>
2023-01-28 05:35:47 +0000

    feat(project): set translate property in figcaption

--- client/src/components/Figure.jsx
+++ client/src/components/Figure.jsx
@@ -7,7 +7,7 @@ export const Figure = ({ idx }) => {
   return (
     <figure className='project-figure'>
       <img src={source} alt={caption + ' image'} className='project-image' srcSet={srcSet} sizes='(max-width: 767px) 500px, (min-width: 768px) and (max-width: 1023px) 1000px, 1500px' />
-      <figcaption className='project-figcaption'>
+      <figcaption className='project-figcaption' translate='no'>
         {caption}
       </figcaption>
     </figure>
