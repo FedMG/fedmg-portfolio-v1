d849687c68009682493035328302cf703552f83f
FedMG <federico.gonzalia@gmail.com>
2023-01-30 03:42:18 +0000

    refactor(typewriter): remove useless typewritter code

--- client/src/components/Typewriter.jsx
+++ client/src/components/Typewriter.jsx
@@ -1,5 +1,4 @@
 import { useTypewriterEffect } from '@/hooks/useTypewriterEffect'
-import { useTypeWriterSplitEffect } from '@/hooks/useTypewriterSplitEffect'
 
 export const Typewriter = ({ msg, delay, timeout, style }) => {
   const [text] = useTypewriterEffect(msg, delay, timeout)
@@ -7,18 +6,6 @@ export const Typewriter = ({ msg, delay, timeout, style }) => {
   return (
     <>
       <span translate='no' className={style}>{text}</span>
-    </>
-  )
-}
-
-export const TypewriterSplit = ({ msg, delay, split, styles }) => {
-  const [text] = useTypeWriterSplitEffect(msg, delay)
-
-  return (
-    <>
-      <span className={styles[0]}>{text.slice(0, split)}
-        <span className={styles[1]}>{text.slice(split)}</span>
-      </span>
     </>
   )
 }
