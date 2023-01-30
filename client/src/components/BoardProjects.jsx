ef3ffe7d20e8ccbb0cde5be05cd280218de0e533
FedMG <federico.gonzalia@gmail.com>
2023-01-30 20:41:54 +0000

    refactor(projects): join files in one and replace useless useContext hook

--- client/src/components/BoardProjects.jsx
+++ client/src/components/BoardProjects.jsx
@@ -1,14 +1,39 @@
-import { createContext } from 'react'
-import { ProjectCard } from './ProjectCard'
+import { Buttons } from "./Buttons";
 
-export const DataContext = createContext()
+const Figure = ({ data: { caption, srcSet, source } }) => {
+  return (
+    <figure className="project-figure">
+        <img
+          src={source}
+          alt={caption + " image"}
+          className="project-image"
+          srcSet={srcSet}
+          sizes="(max-width: 767px) 450px, (min-width: 768px) and (max-width: 1023px) 1000px, 1500px"
+        />
+      <figcaption className="project-figcaption" translate="no">
+        <span className="project-span-caption">{caption}</span>
+      </figcaption>
+    </figure>
+  )
+}
 
 export const BoardProjects = ({ data }) => {
   return data.map(({ id, board }) => (
-    <div key={id} className='project-board'>
-      <DataContext.Provider value={board}>
-        <ProjectCard />
-      </DataContext.Provider>
+    <div key={id} className="project-board">
+      {board.map(({ id, buttons, ...props }) => (
+        <div key={id} className="project-cards">
+          <Figure data={props} />
+          <div className="project-btn-container grid anchor-svg-container">
+            <Buttons
+              data={buttons}
+              style={[
+                "button-visibility anchors",
+                "button project-btn-dimensions",
+              ]}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   ))
 }
