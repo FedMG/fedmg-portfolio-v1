21cb4f651312ccaf40d4da090d23367d99fb2d73
FedMG <federico.gonzalia@gmail.com>
2023-01-31 01:25:55 +0000

    style(projects): added aspect ratio and RD in figures

--- client/src/components/BoardProjects.jsx
+++ client/src/components/BoardProjects.jsx
@@ -3,13 +3,17 @@ import { Buttons } from "./Buttons";
 const Figure = ({ data: { caption, srcSet, source } }) => {
   return (
     <figure className="project-figure">
+      <div className="figure-ar-box-container">
+      <div className="figure-aspect-ratio-box">
         <img
           src={source}
           alt={caption + " image"}
-          className="project-image"
+          className="figure-aspect-ratio"
           srcSet={srcSet}
           sizes="(max-width: 767px) 450px, (min-width: 768px) and (max-width: 1023px) 1000px, 1500px"
         />
+      </div>
+      </div>
       <figcaption className="project-figcaption" translate="no">
         <span className="project-span-caption">{caption}</span>
       </figcaption>
